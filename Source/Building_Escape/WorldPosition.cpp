3ec5f928d9ecac87261bda893156378676da472d
Henry-Ning <henryhuning@gmail.com>
2020-07-24 08:56:48 -0700

    94. Getting An Actor’s Transform 13min

--- Source/Building_Escape/WorldPosition.cpp
+++ Source/Building_Escape/WorldPosition.cpp
@@ -20,8 +20,13 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Object name is %s"), *GetOwner()->GetName());
+	FString ObjectName = GetOwner()->GetName();
+
+	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
+	
+	UE_LOG(LogTemp, Warning, TEXT("%s name in world is %s"), *ObjectName, *ObjectPosition);
+
+
 	
 }
 

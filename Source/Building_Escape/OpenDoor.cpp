44b9f968fd33484651569819f0e078a640726182
Henry-Ning <henryhuning@gmail.com>
2020-08-16 01:41:28 -0700

    111. Getting The Door To Close 5min

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -43,12 +43,24 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	{
 		OpenDoor(DeltaTime);
 	}	
+	else 
+	{
+		CloseDoor(DeltaTime);
+	}
 
 }
 
 void UOpenDoor::OpenDoor(float DeltaTime)
 {
-	CurrentYaw = FMath::Lerp(CurrentYaw, TargetYaw, DeltaTime * 1.f);
+	CurrentYaw = FMath::Lerp(CurrentYaw, TargetYaw, DeltaTime * 0.5f);
+	FRotator DoorRotation = GetOwner() -> GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner() -> SetActorRotation(DoorRotation);
+}
+
+void UOpenDoor::CloseDoor(float DeltaTime)
+{
+	CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * 1.f);
 	FRotator DoorRotation = GetOwner() -> GetActorRotation();
 	DoorRotation.Yaw = CurrentYaw;
 	GetOwner() -> SetActorRotation(DoorRotation);
